2cb2a3f8124aa24a385ee7702cc9d260ac520132
Anatole <allainanatole@gmail.com>
2019-02-11 00:04:25 +0100

    save6

--- src/main/kotlin/kpi/ROIparAppareilparMois.kt
+++ src/main/kotlin/kpi/ROIparAppareilparMois.kt
@@ -4,38 +4,34 @@ import Device
 
 class ROIparAppareilparMois : KPIWork{
     override fun work(devices: List<Device>) {
-        val headers = arrayOf("Appareil", "ROI")
-        val count = devices.groupBy { it.device }.filterValues { it.sumBy { it.cout } > 0 }.count()
-        val data = Array(size = count) { Array(size = 2) { "" } } // String[count][2]
 
-        var index = 0
-        devices.groupBy { it.device }.forEach { device, items ->
-            if (items.sumBy { it.cout } > 0) {
+        val roiparAppareil = devices.groupBy { it.device }
+        roiparAppareil.forEach { device, devices ->
+
+            val roiparmois = devices.groupBy { it.mois }
+
+            val headers = arrayOf("Mois", "ROI")
+            val count = roiparmois.size
+            val data = Array(size = count) { Array(size = 2) { "" } } // String[count][2]
+
+            var index = 0
+            roiparmois.forEach { mois, devicesMois ->
                 val row = data[index]
-                row[0] = "$device"
-                row[1] = "${items.sumBy { it.ca } / items.sumBy { it.cout }}"
-                index++
-            }
-        }
-
-        println(arrayOf(headers, data))
-        println()
-
-        val headers2 = arrayOf("Mois", "ROI")
-        val count2 = devices.groupBy { it.mois }.filterValues { it.sumBy { it.cout } > 0 }.count()
-        val data2 = Array(size = count2) { Array(size = 2) { "" } } // String[count2][2]
-
-        index = 0
-        devices.groupBy { it.mois }.forEach { mois, items ->
-            if (items.sumBy { it.cout } > 0) {
-                val row = data2[index]
                 row[0] = "$mois"
-                row[1] = "${items.sumBy { it.ca } / items.sumBy { it.cout }}"
+                row[1] = "${(devicesMois.sumBy { it.ca })/(devicesMois.sumBy{it.cout}.toDouble())}"
                 index++
             }
-        }
 
-        println(arrayOf(headers2, data2))
-        println()
+            val outerHeaders = arrayOf("Appareil", "ROI par mois")
+            val size = devices.groupBy { it.device }.size
+            val outerData = Array(size = size) { Array(size = 2) { "" } } // String[size][2]
+
+            val row = outerData[0]
+            row[0] = "$device"
+            row[1] = "$headers $data"
+
+            println("$outerHeaders")
+            println("$outerData")
+        }
     }
 }
